9ba63001e47937c80caf7924933eb58555ca8afb
Vilhelm Prytz <vilhelm@prytznet.se>
2020-02-02 21:35:58 +0100

    Experimental write implementations, commented out

--- wilfred/parser/yaml.py
+++ wilfred/parser/yaml.py
@@ -6,7 +6,7 @@
 
 import yaml
 
-from wilfred.core import is_integer, set_in_dict
+# from wilfred.core import is_integer
 
 
 def yaml_read(path):  # this function should be refactored later on!!!
@@ -57,17 +57,79 @@ def yaml_read(path):  # this function should be refactored later on!!!
     return _reformatted
 
 
+# def yaml_write(path, key, value): # does not work yet!!
+#     with open(path) as f:
+#         _raw = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+#     def _traverse(obj, path=None, callback=None):
+#         if path is None:
+#             path = []
+
+#         if type(obj) in [dict]:
+#             value = {k: _traverse(v, path + [k], callback) for k, v in obj.items()}
+#         elif type(obj) in [list, tuple]:
+#             value = [_traverse(elem, path + [[]], callback) for elem in obj]
+#         else:
+#             value = obj
+
+#         if callback is None:
+#             return value
+#         else:
+#             return callback(path, value)
+
+#     def _traverse_modify(obj, target_path, action):
+#         def transformer(path, value):
+#             if path == target_path:
+#                 return action(value)
+#             else:
+#                 return value
+
+#         return _traverse(obj, callback=transformer)
+
+#     def _callback(v):
+#         return value
+
+#     _reformatted_path = [[] if is_integer(x) else x for x in key.split("/")]
+#     _traverse_modify(_raw, _reformatted_path, _callback)
+
+#     # with open(path, "w") as f:
+#         # yaml.dump(_raw, f)
+
+
 def yaml_write(path, key, value):  # does not work yet!!
     with open(path) as f:
         _raw = yaml.load(f.read(), Loader=yaml.FullLoader)
 
-    for x in key.split("/"):
-        if is_integer(x):
-            raise Exception("Wilfred is not able to edit lists")
+    # def _entrypoint(search, current, value, parent):
+    #     if type(value) in [dict]:
+    #         current = current+1
+    #         parent = value
+    #         _iterate_dict(search, current, value, parent)
+    #     if type(value) in [list, tuple]:
+    #         current = current+1
+    #         parent = value
+    #         _iterate_list(search, current, value, parent)
+    #     if type(value) in [str, int, bool]:
+    #         print(f"{search[current]} with value {value} in {parent}")
+    #         exit(1)
 
-    _new = set_in_dict(_raw, key.split("/"), value)
+    # def _iterate_dict(search, current, value, parent):
+    #     for k, v in value.items():
+    #         if k == search[current]:
+    #             _entrypoint(search, current, v, parent)
+
+    # def _iterate_list(search, current, value, parent):
+    #     _entrypoint(search, current, value[search[current]], parent)
+
+    # _search = [int(x) if is_integer(x) else x for x in key.split("/")]
+    # _current = 0
+
+    # for i in range(len(_search)):
+    #     for k, v in _raw.items():
+    #         if _search[_current] == k:
+    #             _entrypoint(_search, _current, v, _raw)
 
     with open(path, "w") as f:
-        yaml.dump(_new, f)
+        yaml.dump(_raw)
 
     return True
