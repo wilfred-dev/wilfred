251077386840fb0462514c535eaf3ef214b19911
Vilhelm Prytz <vilhelm@prytznet.se>
2020-03-08 18:04:16 +0100

    experimental .deb package: update debian/changelog before build

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -16,4 +16,4 @@ fi
 
 # run
 docker run --volume=$(pwd)/debian/dist:/tmp/wilfred-deb-pkg/debian/dist wilfred-deb-pkg \
-    /bin/bash -c "sed -i "s/development/`git log -1 --format="%H"`/g" wilfred/version.py &&  sed -i "s/YYYY-MM-DD/`git log -1 --format="%at" | xargs -I{} date -d @{} +%Y-%m-%d`/g" wilfred/version.py && dpkg-buildpackage -us -uc -b && mv /tmp/wilfred_*.deb /tmp/wilfred-deb-pkg/debian/dist"
+    /bin/bash -c "sed -i "s/development/`git log -1 --format="%H"`/g" wilfred/version.py && sed -i "s/YYYY-MM-DD/`git log -1 --format="%at" | xargs -I{} date -d @{} +%Y-%m-%d`/g" wilfred/version.py && python3 scripts/debian_pkg_format_changelog.py && dpkg-buildpackage -us -uc -b && mv /tmp/wilfred_*.deb /tmp/wilfred-deb-pkg/debian/dist"
