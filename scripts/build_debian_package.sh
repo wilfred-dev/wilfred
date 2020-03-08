289494ee1e6c796c4e597640bd7ca855c328228b
Vilhelm Prytz <vilhelm@prytznet.se>
2020-03-08 16:27:09 +0100

    experimental: set version in wilfred/version.py for deb package

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -11,9 +11,9 @@ if [ ! -d ./debian/dist ]; then
 fi
 
 if [ -e ./debian/dist/*.deb ]; then
-    sudo rm ./debian/dist/*.deb
+    rm ./debian/dist/*.deb
 fi
 
 # run
 docker run --volume=$(pwd)/debian/dist:/tmp/wilfred-deb-pkg/debian/dist wilfred-deb-pkg \
-    /bin/bash -c "dpkg-buildpackage -us -uc -b && mv /tmp/wilfred_*.deb /tmp/wilfred-deb-pkg/debian/dist"
+    /bin/bash -c "sed -i "s/development/`git log -1 --format="%H"`/g" wilfred/version.py &&  sed -i "s/YYYY-MM-DD/`git log -1 --format="%at" | xargs -I{} date -d @{} +%Y-%m-%d`/g" wilfred/version.py && dpkg-buildpackage -us -uc -b && mv /tmp/wilfred_*.deb /tmp/wilfred-deb-pkg/debian/dist"
