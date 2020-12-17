002dc931cccf5cd5c711f8021025c4802192fa04
Vilhelm Prytz <vilhelm@prytznet.se>
2020-12-17 21:06:34 +0100

    docs: use sphinx-click over handwritten documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = "v0.7.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_click"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
