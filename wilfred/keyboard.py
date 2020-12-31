b3bd7f8de1b03a2c35fbe1874b03aff64fab13e2
Vilhelm Prytz <vilhelm@prytznet.se>
2021-01-01 00:27:00 +0100

    Update all copyright notices to 2021
    Happy new year!

--- wilfred/keyboard.py
+++ wilfred/keyboard.py
@@ -1,12 +1,12 @@
-####################################################################
-#                                                                  #
-# Wilfred                                                          #
-# Copyright (C) 2020, Vilhelm Prytz, <vilhelm@prytznet.se>, et al. #
-#                                                                  #
-# Licensed under the terms of the MIT license, see LICENSE.        #
-# https://github.com/wilfred-dev/wilfred                           #
-#                                                                  #
-####################################################################
+#########################################################################
+#                                                                       #
+# Wilfred                                                               #
+# Copyright (C) 2020-2021, Vilhelm Prytz, <vilhelm@prytznet.se>, et al. #
+#                                                                       #
+# Licensed under the terms of the MIT license, see LICENSE.             #
+# https://github.com/wilfred-dev/wilfred                                #
+#                                                                       #
+#########################################################################
 
 import threading
 
