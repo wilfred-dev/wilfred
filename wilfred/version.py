b1836e6385e9a96e8e9bba97127daa41a3e1d6c8
Vilhelm Prytz <vilhelm@prytznet.se>
2022-05-08 17:13:10 +0200

    Set version for development
    [skip ci]

--- wilfred/version.py
+++ wilfred/version.py
@@ -11,6 +11,6 @@
 # use "0.0.0.dev0" to indicate incremental commit build
 # specify version "0.3.0" to indicate standard release
 # "commit_date" should be replaced upon building
-version = "0.10.1"
+version = "0.0.0.dev0"
 commit_hash = "development"
 commit_date = "YYYY-MM-DD"
